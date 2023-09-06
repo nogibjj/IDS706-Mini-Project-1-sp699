199d8660a61412561056b6ab466c6d5e1042ab60
suim.park <suim.park@duke.edu>
2023-09-05 22:11:48 -0400

    -

--- test_main.py
+++ test_main.py
@@ -1,10 +1,11 @@
-"""
-Test goes here
+# Test File
 
-"""
-
-from mylib.calculator import add
+from main import add
 
 
 def test_add():
     assert add(1, 2) == 3
+
+
+if __name__ == "__main__":
+    test_add()
